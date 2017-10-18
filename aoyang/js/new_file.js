e6b8fbedc25b9df5ed50c176dfb16970507726dd
xuleo <31871874+xuleo@users.noreply.github.com>
2017-10-18 10:03:12 +0800

    Add files via upload

---
+++ aoyang/js/new_file.js
@@ -0,0 +1,54 @@
+window.onload=function () {
+	$(".lunbo_dian_2").click(function  () {
+		$(".lunbo").animate({left:-$(".lunbo li").width() });
+		$(".lunbo_dian span").css("background-color","#d5d5d5");
+		$(".lunbo_dian_2").css("background-color","#f39800");
+	})
+	$(".lunbo_dian_1").click(function  () {
+		$(".lunbo").animate({left:0 });
+		$(".lunbo_dian span").css("background-color","#d5d5d5");
+		$(".lunbo_dian_1").css("background-color","#f39800");
+	})
+	$(".lunbo_dian_3").click(function  () {
+		$(".lunbo").animate({left:-$(".lunbo li").width()*2 });
+		$(".lunbo_dian span").css("background-color","#d5d5d5");
+		$(".lunbo_dian_3").css("background-color","#f39800");
+	})
+	$(".lunbo_dian_4").click(function  () {
+		$(".lunbo").animate({left:-$(".lunbo li").width()*3 });
+		$(".lunbo_dian span").css("background-color","#d5d5d5");
+		$(".lunbo_dian_4").css("background-color","#f39800");
+	})
+	var a=1;
+	function move() {
+		$(".lunbo").animate({left:-$(".lunbo li").width()*a },function  () {
+			$(".lunbo_dian span").css("background-color","#d5d5d5");
+			if (a==1) {
+				a++;
+				$(".lunbo_dian :nth-child(2)").css("background-color","#f39800");
+			}else if(a==2){
+				a++;
+				$(".lunbo_dian :nth-child(3)").css("background-color","#f39800");
+			}
+			else if(a==0){
+				a++;
+				$(".lunbo_dian :nth-child(1)").css("background-color","#f39800");
+			}
+			else{
+				a=0;
+				$(".lunbo_dian :nth-child(4)").css("background-color","#f39800");
+			}
+		});
+	}
+	var timer=setInterval(move,2000);
+	$(".lunbo").hover(function () {
+		clearInterval(timer);
+	},function () {
+		timer=setInterval(move,2000);
+	})
+	$(".lunbo_dian").hover(function () {
+		clearInterval(timer);
+	},function () {
+		timer=setInterval(move,2000);
+	})
+}
